1f3a5ca8cfb1274c683ca52944d034060ddf9995
ladcs <hpladcs@gmail.com>
2022-06-30 20:35:13 -0300

    [fix] docker

--- backend/src/database/config/database.ts
+++ backend/src/database/config/database.ts
@@ -2,11 +2,11 @@ import 'dotenv/config';
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
-  database: process.env.DB_NAME || 'trace',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3002,
+  username: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASS|| '123456',
+  database: process.env.MYSQL_NAME || 'trace',
+  host: process.env.MYSQL_HOST || 'mysql',
+  port: Number(process.env.MYSQL_PORT) || 3002,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
