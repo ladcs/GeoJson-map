b47de64b4984e30bf3fbaf1b3184ddb7aeef24e8
ladcs <hpladcs@gmail.com>
2022-07-05 13:39:13 -0300

    [feat] delete point frontend

--- frontend/src/components/mapCollectionPoints.jsx
+++ frontend/src/components/mapCollectionPoints.jsx
@@ -1,10 +1,19 @@
 import React, { useContext, useEffect } from 'react';
+import { FiDelete } from 'react-icons/fi';
 import { Marker, Popup } from 'react-leaflet';
 import api from '../services/api';
 import MapContext from '../context/mapContext';
-
 function MapCollectionPoints() {
   const { points, setPoints } = useContext(MapContext);
+  const deletePoint= (id) => {
+    api.delete(`/point/${id}`).then(() =>{
+      api.get('/point').then(res => {
+        const { data } = res;
+        if(!data) return;
+        setPoints(data);
+      });
+    });
+  }
   useEffect(() => {
     api.get('/point').then(res => {
       const { data } = res;
@@ -27,7 +36,12 @@ function MapCollectionPoints() {
         position={ p.geometry.coordinates }
         key={ p.geometry.coordinates }>
           <Popup>
+            <div className='popup'>
             { p.properties.name }
+            <button onClick={ () => {
+              deletePoint(p.properties._id)
+            } }><FiDelete/></button>
+            </div>
           </Popup>
         </Marker>
         )
